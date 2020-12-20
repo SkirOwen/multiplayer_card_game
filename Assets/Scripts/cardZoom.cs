47b651af0905740fdac1fa5756153a8d88c48fe8
Owen <owen.allemang@gmail.com>
2020-12-20 18:32:53 +0000

    first commit
    first commit

---
+++ Assets/Scripts/cardZoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardZoom : MonoBehaviour
+{
+	public GameObject Canvas;
+	private GameObject zoomCard;
+
+	public void Awake()
+	{
+		Canvas = GameObject.Find("Main Canvas");
+	}
+
+	public void OnHoverEnter()
+	{
+		zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 250), Quaternion.identity);
+		zoomCard.transform.SetParent(Canvas.transform, false);
+
+		RectTransform rect = zoomCard.GetComponent<RectTransform>();
+		rect.sizeDelta = new Vector2(210, 285);
+	}
+
+	public void OnHoverExit()
+	{
+		Destroy(zoomCard);
+	}
+}
